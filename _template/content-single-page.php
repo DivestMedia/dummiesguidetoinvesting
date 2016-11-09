7a1d2f3042eb9db21762a1fd8a6f724ddd204a59
ljopleda <ljopleda@gmail.com>
2016-11-09 14:37:50 +0800

    Single page

--- _template/content-single-page.php
+++ _template/content-single-page.php
@@ -3,35 +3,41 @@ get_header();
 ?>
 
 	<section>
-		<header class="text-center margin-bottom-30">
-			<h2 class="section-title"><?=the_title()?></h2>
-		</header>
 		<div class="container">
-			<?php
-					// Start the Loop.
-			while ( have_posts() ) : the_post();
-			?>
-				<article id="post-<?php the_ID(); ?>">
-					<div class="text-black size-14 entry-content post-<?=get_post_format();?>">
-						
-						<!-- article content -->
-						<? the_content();?>
-						<!-- /article content -->
-							
-						<?
-						wp_link_pages( array( 
-							'before' => '<div class="divider"></div><ul class="pagination pagination-md"><h4>Pages</h4>', 
-							'after' => '</ul>',
-							//'pagelink' => '%' 
-						) );
-				
-					?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-## -->
-			<?php endwhile; // end of the loop. ?>
+			<div class="col-md-9">
+				<header class="margin-bottom-30">
+					<h2 class="section-title"><?=the_title()?></h2>
+				</header>
+				<?php
+						// Start the Loop.
+				// while ( have_posts() ) : the_post();
+				?>
+					<article id="post-<?=$post->ID?>">
+						<div class="text-black size-14 entry-content post-<?=get_post_format($post->ID);?>">
+
+							<!-- article content -->
+							<?=wpautop($post->post_content);?>
+							<!-- /article content -->
+
+							<?
+							wp_link_pages( array(
+								'before' => '<div class="divider"></div><ul class="pagination pagination-md"><h4>Pages</h4>',
+								'after' => '</ul>',
+								//'pagelink' => '%'
+							) );
+
+						?>
+						</div><!-- .entry-content -->
+					</article><!-- #post-## -->
+				<?php //endwhile; // end of the loop. ?>
+			</div>
+			<div class="col-md-3">
+				<?php
+					if(is_active_sidebar('sidebar-single'))
+						dynamic_sidebar('sidebar-single');
+				?>
+			</div>
 		</div>
 	</section>
 <?
 get_footer();
-
-			
